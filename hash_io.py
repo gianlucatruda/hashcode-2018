7011e2d6aa5174a5ee5fd88b7b829010107a29df
Gianluca Truda <gianlucatruda@gmail.com>
2018-03-01 20:25:17 +0200

    Added io library.

---
+++ hash_io.py
@@ -0,0 +1,51 @@
+
+'''
+Takes in filename as a string and returns array formatted as
+[rows, columns, cars, bonus, steps, <list of rides>]
+'''
+def read_file(fname):
+	if fname == "":
+		fname = "a_example.in"
+	file = open(fname, "r")
+	line = file.readline()
+
+	params = line.split(" ")
+	rows = int(params[0])
+	cols = int(params[1])
+	cars = int(params[2])
+	rides = int(params[3])
+	bonus = int(params[4])
+	steps = int(params[5])
+
+	ride_list = []
+
+	for i in range(rides):
+		params = file.readline().split(" ")
+		start_point = [int(params[0]), int(params[1])]
+		end_point = [int(params[2]), int(params[3])]
+		early_start = int(params[4])
+		late_finish = int(params[5])
+		ride_list.append([i, start_point, end_point, early_start, late_finish])
+
+	file.close()
+	return [rows, cols, cars, bonus, steps, ride_list]
+
+'''
+Takes in a name for the output file (will overwrite), and a list of lists
+corresponding to each vehicles rides.
+'''
+def write_file(fname, vehicles):
+	out_text = ""
+	for v in vehicles:
+		out_text += str(len(v))
+		for i in range(len(v)):
+			out_text += " "+str(v[i])
+		out_text += "\n"
+	out_file = open(fname, "wb")
+	out_file.write(bytes(out_text, 'UTF-8'))
+	out_file.close()
+	return True
+
+if __name__ == "__main__":
+	write_file("test.out", [[0], [2,1]])
+
